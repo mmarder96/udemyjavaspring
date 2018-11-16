f86dfe208b24abe2cecd9931a4527f3c4a192d76
mmarder96 <mmarder96@gmail.com>
2018-11-15 23:10:14 -0800

    created bean scope demo and began bean life cycle demo

--- spring-demo-one/src/com/luv2code/springdemo/SwimCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/SwimCoach.java
@@ -1,15 +1,15 @@
 package com.luv2code.springdemo;
 
-public class SwimCoach implements Coach{
-	
+public class SwimCoach implements Coach {
+
 	// define a private field for the dependency
 	private FortuneService fortuneService;
-	
+
 	// define a constructor for dependency injection
 	public SwimCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
 	}
-	
+
 	@Override
 	public String getDailyWorkout() {
 		return "Swim 20 laps";
@@ -18,7 +18,16 @@ public class SwimCoach implements Coach{
 	@Override
 	public String getDailyFortune() {
 		// use my fortuneService to get a fortune
-		return "Swim because " + fortuneService.getFortune();
+		return "Swim today because " + fortuneService.getFortune();
 	}
 
+	// add an init method
+	public void startupStuff() {
+		System.out.println(this.getClass().getSimpleName() + ": inside method startupStuff");
+	}
+
+	// add a destroy method
+	public void cleanupStuff() {
+		System.out.println(this.getClass().getSimpleName() + ": inside method cleanupStuff");
+	}
 }
