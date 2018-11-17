40567e09fb2b9b1a232f5633e735fb164df85c3a
mmarder96 <mmarder96@gmail.com>
2018-11-17 13:56:14 -0800

    implemented qualifier annotation for dependency injection

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/RandomFortuneService.java
@@ -0,0 +1,27 @@
+package com.luv2code.springdemo;
+
+import java.util.Random;
+
+public class RandomFortuneService implements FortuneService {
+	
+	// create random number generator
+	Random rand = new Random();
+	
+	// create array of fortunes
+	String[] fortunes = { 
+		"Today is your lucky day!",
+		"A beautiful, smart, and loving person will be coming into your life.",
+		"A dubious friend may be an enemy in camouflage.", "A faithful friend is a strong defense.",
+		"A feather in the hand is better than a bird in the air.", "A fresh start will put you on your way.",
+		"A friend asks only for your time not your money.",
+		"A gambler not only will lose what he has, but also will lose what he doesn’t have."
+	};
+
+	@Override
+	public String getFortune() {
+		// return random fortune
+		int randIdx = rand.nextInt(fortunes.length);
+		return fortunes[randIdx];
+	}
+
+}
--- spring-demo-annotations/src/com/luv2code/springdemo/TrackCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TrackCoach.java
@@ -2,6 +2,7 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
 
 
 @Component
@@ -10,6 +11,7 @@ public class TrackCoach implements Coach {
 	private FortuneService fortuneService;
 	
 	@Autowired
+//	@Qualifier("randomFortuneService")
 	public TrackCoach(FortuneService myFortuneService) {
 		fortuneService = myFortuneService;
 	}
