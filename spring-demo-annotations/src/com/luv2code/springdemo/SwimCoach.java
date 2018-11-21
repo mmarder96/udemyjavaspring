4b9b044ee50a28131d7b9ac8f2339ba36a566676
Maxwell Marder <mmarder@westlakefinancial.com>
2018-11-21 13:40:05 -0800

    added value injection to java config demo app

--- spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
@@ -1,10 +1,24 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
     private FortuneService fortuneService;
+
+    @Value("${foo.email}")
     private String email;
+
+    @Value("${foo.team}")
     private String team;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTeam() {
+        return team;
+    }
 
     public SwimCoach(FortuneService myFortuneService) {
         fortuneService = myFortuneService;
--- spring-demo-annotations/src/com/luv2code/springdemo/SwimCoachJavaConfigDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimCoachJavaConfigDemoApp.java
@@ -3,23 +3,25 @@ package com.luv2code.springdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SwimCoachJavaConfigDemoApp {
-	
-	public static void main(String[] args) {
-		
-		// read spring config class
-		AnnotationConfigApplicationContext context =
-				new AnnotationConfigApplicationContext(MyLoggerJavaConfig.class, SportConfig.class);
-		
-		// get the bean from the spring container
-		Coach myCoach = context.getBean("swimCoach", Coach.class);
-		
-		// call methods on the bean
-		System.out.println(myCoach.getDailyWorkout());
-		System.out.println(myCoach.getDailyFortune());
 
-		// close the context
-		context.close();
-		
-	}
+    public static void main(String[] args) {
+
+        // read spring config class
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyLoggerJavaConfig.class, SportConfig.class);
+
+        // get the bean from the spring container
+        SwimCoach myCoach = context.getBean("swimCoach", SwimCoach.class);
+
+        // call methods on the bean
+        System.out.println(myCoach.getDailyWorkout());
+        System.out.println(myCoach.getDailyFortune());
+        System.out.println("email: " + myCoach.getEmail());
+        System.out.println("team: " + myCoach.getTeam());
+
+        // close the context
+        context.close();
+
+    }
 
 }
