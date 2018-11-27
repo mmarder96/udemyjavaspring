8cdae2ad24a651d1529f1310f42a7a60894bf4fd
mmarder96 <mmarder96@gmail.com>
2018-11-26 16:12:21 -0800

    implemented @InitBinder to filter form data

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,8 +1,11 @@
 package com.luv2code.springdemo.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,11 +15,19 @@ import javax.validation.Valid;
 @RequestMapping("/customer")
 public class CustomerController {
 
+    // add an initbinder to convert/trim input strings
+    // remove leading and trailing whitespace
+    // resolve validation issues
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+
     @RequestMapping("/showForm")
     public String showForm(Model myModel) {
-
         myModel.addAttribute("customer", new Customer());
-
         return "customer-form";
     }
 
